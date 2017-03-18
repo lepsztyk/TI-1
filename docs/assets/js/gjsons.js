89ae4721241d451f143af929d0ee36b0b4558df7
sbilikiewicz <arcziy.pro@gmail.com>
2017-03-18 21:02:15 +0100

    Update Geojson

--- docs/assets/js/gjsons.js
+++ docs/assets/js/gjsons.js
@@ -3,7 +3,7 @@ var geojsons = [
         "type": "Feature",
         "geometry": {
             "type": "Point",
-            "coordinates": [120.98, 14.59]
+            "coordinates": [48.40, 14.59]
         },
         "properties": {
             "name": "Manila"
@@ -13,35 +13,10 @@ var geojsons = [
         "type": "Feature",
         "geometry": {
             "type": "Point",
-            "coordinates": [100.49, 13.76]
+            "coordinates": [86.49, 13.76]
         },
         "properties": {
             "name": "Bangkok"
         }
     },
-    {
-        "type": "Feature",
-        "geometry": {
-            "type": "LineString",
-            "coordinates": [
-                [120.98, 14.59], [100.49, 13.76]
-            ]
-        },
-        "properties": {
-            "name": "From Manila to Bangkok"
-        }
-    },
-    {
-        "type": "Feature",
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [
-                [ [120.0, 14.50], [121.50, 14.50], [121.50, 16.20],
-                [120.0, 16.20], [120.0, 14.50] ]
-            ]
-        },
-        "properties": {
-            "name": "Central Luzon Philippines"
-        }
-    }
 ];
